4297e284bc89c0e697f78e288fb969f4b14efb94
rewatevijaykumar <rewatevijaykumar@gmail.com>
2022-09-22 15:40:56 +0530

    initial

---
+++ app.py
@@ -0,0 +1,49 @@
+from tkinter.tix import DisplayStyle
+import pandas as pd
+import numpy as np
+from flask import Flask, render_template, request
+from pickle import dump
+from pickle import load
+import json
+app = Flask(__name__)
+
+# Reading the data
+data=pd.read_csv("Gold_data.csv")
+data['date'] = pd.to_datetime(data['date'])
+data.set_index('date',inplace=True)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        print(request.form.get('days'))
+        try:
+            days = int(request.form['days'])
+                        
+            # Loading the model we already created
+            model=load(open('hwf.sav','rb'))
+            predictions=model.forecast(days)
+            pred_df=pd.DataFrame({"Prediction":predictions})
+            pred_df.insert(loc=0, column='Date', value=pred_df.index.strftime('%d/%m/%Y'))
+            pred_df = pred_df.round(2)
+            pred_df = pred_df.reset_index(drop=True)
+            pred_df.index = np.arange(1, len(pred_df) + 1)
+            json_records = pred_df.reset_index().to_json(orient ='records')
+            data = []
+            data = json.loads(json_records)
+            print(pred_df)
+        except ValueError:
+            return render_template('home.html',error = "Please enter valid values")
+    return render_template('home.html',
+    column_names=['Number of Day','Date','Price'], 
+    data=data,
+    legend = f'Gold Forecast for {days} days',
+    labels=list(pred_df['Date'].values.tolist()), 
+    values = list(pred_df['Prediction'].values.tolist()))
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./templates/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'body': ['montserrat', 'roboto', 'sans-serif'],
+            },
+        }
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        // ...
+    ],
+}
